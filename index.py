983b14d818e976db45f660f6245203409b9ae258
BrianMwangi21 <mwangibrian8@gmail.com>
2020-02-19 11:13:44 +0300

    We are now scrapping

---
+++ index.py
@@ -0,0 +1,36 @@
+# Using GetOldTweets3 library instead of Tweepy because of query limits
+import GetOldTweets3 as got
+import pandas as pd
+
+
+def text_query(text_to_query, count, since_date, until_date, location):
+    # Creation of query object
+    tweet_criteria = got.manager.TweetCriteria().setQuerySearch(text_to_query).\
+                                                 setMaxTweets(count).\
+                                                 setSince(since_date).\
+                                                 setUntil(until_date).\
+                                                 setNear(location).\
+                                                 setEmoji("unicode")
+
+    # Creation of list that contains all tweets from query
+    tweets_data = got.manager.TweetManager.getTweets(tweet_criteria)
+
+    # Creating list of chosen tweet data
+    tweets = []
+
+    # Format data before saving
+    for tweet in tweets_data:
+        date = tweet.date.strftime('%Y-%m-%d')
+        time = tweet.date.strftime('%H:%M')
+        tweets.append([date, time, tweet.username, tweet.text, tweet.permalink])
+
+    # Creation of dataframe from tweets
+    tweets_df = pd.DataFrame(tweets, columns=['Date', 'Time', 'Username', 'Tweet', 'Permalink'])
+
+    # Converting tweets dataframe to csv file
+    tweets_df.to_csv('{}-tweets.csv'.format(text_to_query), sep=',')
+
+
+# Selected hashtag is #IkoKazeKe
+# Selected time interval is from January 01 2019 to December 31 2019
+text_query("#IkoKaziKe", 5000, "2019-01-01", "2019-12-31", "Nairobi, Kenya")
